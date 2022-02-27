c623d905e66b25c385c523a350fd9be8c86db148
Yoo Hyun Sik <dbgustlr92@gmail.com>
2022-02-27 12:04:16 +0900

    Initial commit

---
+++ 3dollar-in-my-pocket-manager/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  3dollar-in-my-pocket-manager
+//
+//  Created by Hyun Sik Yoo on 2022/02/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
