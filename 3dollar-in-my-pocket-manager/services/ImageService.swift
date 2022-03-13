ed6b96f7ad471e31bbd05e8ded7cc1894a5c1900
Yoo Hyun Sik <dbgustlr92@gmail.com>
2022-03-13 20:56:31 +0900

    feat: 이미지 업로드 API 오류 해결

--- 3dollar-in-my-pocket-manager/services/ImageService.swift
+++ 3dollar-in-my-pocket-manager/services/ImageService.swift
@@ -2,6 +2,7 @@ import UIKit
 
 import Alamofire
 import RxSwift
+import Base
 
 protocol ImageServiceType {
     func uploadImage(image: UIImage, fileType: FileType) -> Observable<ImageUploadResponse>
@@ -16,7 +17,14 @@ struct ImageService: ImageServiceType {
         return .create { observer in
             let urlString = HTTPUtils.url + "/boss/v1/upload/\(fileType.rawValue)"
             
-            HTTPUtils.fileUploadSession.upload(data, to: urlString)
+            HTTPUtils.fileUploadSession.upload(multipartFormData: { multipartFormData in
+                multipartFormData.append(
+                    data,
+                    withName: "file",
+                    fileName: DateUtils.todayString(format: "yyyy-MM-dd'T'HH-mm-ss") + "_image.png",
+                    mimeType: "image/png"
+                )
+            }, to: urlString)
                 .responseDecodable(of: ResponseContainer<ImageUploadResponse>.self) { response in
                     if response.isSuccess() {
                         observer.processValue(response: response)
