5452dcb48a65b746bfd64ad67553431bc3cfd76b
Hyunsik Yoo <dbgustlr92@gmail.com>
2022-06-07 21:55:25 +0900

    feat: 로그인 화면 이미지 적용

--- 3dollar-in-my-pocket-manager/domains/membership/signin/SigninView.swift
+++ 3dollar-in-my-pocket-manager/domains/membership/signin/SigninView.swift
@@ -3,19 +3,32 @@ import UIKit
 import Base
 
 final class SigninView: BaseView {
+    private let introImageView = UIImageView().then {
+        $0.image = UIImage(named: "img_intro")
+        $0.contentMode = .scaleAspectFill
+    }
+    
     let appleButton = SocialSigninButton(socialType: .apple)
     
     let kakaoButton = SocialSigninButton(socialType: .kakao)
     
     override func setup() {
-        self.backgroundColor = .gray0
+        self.backgroundColor = .gray100
         self.addSubViews([
+            self.introImageView,
             self.appleButton,
             self.kakaoButton
         ])
     }
     
     override func bindConstraints() {
+        self.introImageView.snp.makeConstraints { make in
+            make.top.equalTo(self.safeAreaLayoutGuide).offset(106)
+            make.left.equalToSuperview()
+            make.right.equalToSuperview()
+            make.height.equalTo(self.introImageView.snp.height)
+        }
+        
         self.kakaoButton.snp.makeConstraints { make in
             make.left.equalToSuperview().offset(32)
             make.right.equalToSuperview().offset(-32)
