d27db4c3028041cc563c64a501ba450a86324028
Hyunsik Yoo <dbgustlr92@gmail.com>
2024-07-27 21:55:20 +0900

    TH-236 : UIImageView+ 파일 하나로 정리

--- 3dollar-in-my-pocket-manager/extensions/UIImageView+.swift
+++ 3dollar-in-my-pocket-manager/extensions/UIImageView+.swift
@@ -7,4 +7,11 @@ extension UIImageView {
         image = nil
         kf.cancelDownloadTask()
     }
+    
+    func setImage(urlString: String?) {
+        if let urlString = urlString,
+           let url = URL(string: urlString) {
+            self.kf.setImage(with: url, options: [.transition(.fade(0.2))])
+        }
+    }
 }
